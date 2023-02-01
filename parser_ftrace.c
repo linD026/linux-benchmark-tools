140f40d7fff5ce0273b11ea912fdba5e44e05ea7
Chih-En Lin <shiyn.lin@gmail.com>
2023-02-01 21:09:31 +0800

    Fix some output errors

--- parser_ftrace.c
+++ parser_ftrace.c
@@ -13,8 +13,8 @@
  *
  */
 
-#define FTRACE_OUPUT "ftrace_output"
-//#define FTRACE_OUPUT "triforce-fuzzer.log"
+//#define FTRACE_OUPUT "ftrace_output"
+#define FTRACE_OUPUT "triforce-fuzzer.log"
 
 #define STACK_INFO_SIZE 80
 struct info {
@@ -30,22 +30,36 @@ struct info {
     int maybe_nested;
 };
 
-#define MAX_TASK_SIZE 256
+#define MAX_TASK_SIZE 512
 struct task {
     /* curr_info == current head */
     struct info *curr_info;
     /* task main head */
     struct info *head;
     unsigned int pid;
+    char name[40];
 };
+
 static struct task tasks[MAX_TASK_SIZE];
 static unsigned int task_top = 0;
 static struct task *current;
 
+static unsigned int *prev_cpu;
+
 #define BUF_MAX_SIZE 256
+static char buffer[BUF_MAX_SIZE];
 
 static int chart_cnt = 0;
 static FILE *fp = NULL;
+
+static void check_and_update_prev_cpu(struct info *info)
+{
+    if (prev_cpu)
+        BUG_ON(info->cpu != *prev_cpu,
+               "Potentially miss something plz check manually\nbuffer:%s",
+               buffer);
+    prev_cpu = &info->cpu;
+}
 
 static struct info *dup_main_info(void)
 {
@@ -63,8 +77,8 @@ static struct info *dup_main_info(void)
     return tmp;
 }
 
-static void switch_task(struct info **head, unsigned int curr_pid,
-                        unsigned int next_pid)
+static int switch_task(struct info **head, const char *next_name,
+                       unsigned int next_pid)
 {
     current->curr_info = *head;
 
@@ -72,23 +86,27 @@ static void switch_task(struct info **head, unsigned int curr_pid,
         if (next_pid == tasks[i].pid) {
             *head = tasks[i].curr_info;
             current = &tasks[i];
-            return;
+            return 0;
         }
     }
-    BUG_ON(task_top >= MAX_TASK_SIZE, "max task:%d/%d", task_top,
-           MAX_TASK_SIZE);
+    if (task_top >= MAX_TASK_SIZE) {
+        // we will output warning at the end.
+        return 1;
+    }
+
+    strncpy(tasks[task_top].name, next_name, strlen(next_name));
     tasks[task_top].pid = next_pid;
     tasks[task_top].head = dup_main_info();
     tasks[task_top].curr_info = tasks[task_top].head;
     *head = tasks[task_top].curr_info;
     current = &tasks[task_top];
     task_top++;
+
+    return 0;
 }
 
 static struct info *parser(struct info *head)
 {
-    char buffer[BUF_MAX_SIZE] = { 0 };
-
     while (memset(buffer, '\0', BUF_MAX_SIZE),
            fgets(buffer, BUF_MAX_SIZE, fp) != NULL) {
         char us[5];
@@ -110,6 +128,8 @@ static struct info *parser(struct info *head)
                 info->stack_info[i] = head->stack_info[i];
         }
 
+        //printf("> %s", buffer);
+
         /*
          *   + means that the function exceeded 10 usecs.
          *   ! means that the function exceeded 100 usecs.
@@ -124,6 +144,7 @@ static struct info *parser(struct info *head)
         ret =
             sscanf(buffer, "%d%c %c %s %c", &info->cpu, &l, &t, info->name, &r);
         if (ret == 5 && l == ')' && t == '|' && r == '{') {
+            check_and_update_prev_cpu(info);
             if (head) {
                 offset = strlen(info->name);
                 info->name[offset - 2] = '\0';
@@ -164,6 +185,7 @@ static struct info *parser(struct info *head)
                      &info->nr, us, &t, &r);
         if (ret == 7 && l == ')' && in_sym_list(a) && t == '|' && r == '}') {
         end:
+            check_and_update_prev_cpu(info);
             head->nr += info->nr;
             free(info);
             break;
@@ -192,32 +214,41 @@ static struct info *parser(struct info *head)
             char curr_name[40];
             char next_name[40];
 
+            // cleanup the cpu checking info
+            prev_cpu = NULL;
+
             memset(buffer, '\0', BUF_MAX_SIZE);
             memset(curr_name, '\0', 40);
             memset(next_name, '\0', 40);
             BUG_ON(!fgets(buffer, BUF_MAX_SIZE, fp), "fget() == NULL");
             ret = sscanf(buffer, "%d%c %s %s %s", &next_cpu, &l, curr_name,
                          arrow, next_name);
+            //printf("> %s", buffer);
             BUG_ON(ret != 5, "switch task:|%s|%s|%s|", buffer, curr_name,
                    next_name);
             for (int i = 0; i < 37; i++) {
                 if (curr_name[i] == '>' && curr_name[i + 1] == '-') {
                     curr_pid = atoi(&curr_name[i + 2]);
+                    curr_name[i + 1] = '\0';
                     break;
                 }
             }
             for (int i = 0; i < 37; i++) {
                 if (next_name[i] == '>' && next_name[i + 1] == '-') {
                     next_pid = atoi(&next_name[i + 2]);
+                    next_name[i + 1] = '\0';
                     break;
                 }
             }
 
             free(info);
             BUG_ON(!current, "current == NULL");
-            if (current->pid == 0)
+            if (current->pid == 0) {
+                strncpy(current->name, curr_name, strlen(curr_name));
                 current->pid = curr_pid;
-            switch_task(&head, curr_pid, next_pid);
+            }
+            if (switch_task(&head, next_name, next_pid))
+                break;
 
             //skip next split line
             BUG_ON(!fgets(buffer, BUF_MAX_SIZE, fp), "fget() == NULL");
@@ -229,6 +260,8 @@ static struct info *parser(struct info *head)
 
     success:
         BUG_ON(!head, "head == NULL with info:%s", buffer);
+        check_and_update_prev_cpu(info);
+
         offset = strlen(info->name);
         info->name[offset - name_rollback] = '\0';
         for (struct info *tmp = head->nested ? head->nested : head->next; tmp;
@@ -260,21 +293,20 @@ static void print_ftrace_info(void)
             time += tmp->nr;
     }
 
+    printf("drawChart(");
     printf("'ftrace - %f us',\n", time);
     printf("          [['ftrace', 'duration (us)'],\n");
 
     for (int i = 0; i < task_top; i++) {
-        for (struct info *tmp = tasks[i].head->nested ? tasks[i].head->nested :
-                                                        tasks[i].head->next;
-             tmp;) {
+        for (struct info *tmp = tasks[i].head->next; tmp;) {
             struct info *next = tmp->next;
 
-            if (!next)
-                printf("           ['PID<%d>:%s', %f]],\n", tasks[i].pid,
-                       tmp->name, tmp->nr);
+            if (!next && i + 1 == task_top)
+                printf("           ['%s-PID<%d>:%s', %f]],\n", tasks[i].name,
+                       tasks[i].pid, tmp->name, tmp->nr);
             else
-                printf("           ['PID<%d>%s', %f],\n", tasks[i].pid,
-                       tmp->name, tmp->nr);
+                printf("           ['%s-PID<%d>%s', %f],\n", tasks[i].name,
+                       tasks[i].pid, tmp->name, tmp->nr);
             free(tmp);
             tmp = next;
         }
@@ -282,19 +314,48 @@ static void print_ftrace_info(void)
     printf("          \"piechart%d\");\n", chart_cnt++);
 }
 
-static void __print_info(struct info *head, unsigned int pid)
+static void print_per_task_ftrace_info(struct info *head, const char *name,
+                                       unsigned int pid)
 {
     float time = 0;
 
-    printf("drawChart(");
-    if (strncmp(head->name, "ftrace", sizeof("ftrace")) == 0)
-        return;
+    for (struct info *tmp = head->next; tmp; tmp = tmp->next)
+        time += tmp->nr;
 
-    printf("'PID<%d>:", pid);
+    printf("drawChart(");
+    printf("'%s-PID<%d>:", name, pid);
+    printf("%s - %f us',\n", head->name, time);
+    printf("          [['%s', 'duration (us)'],\n", head->name);
+
+    for (struct info *tmp = head->nested ? head->nested : head->next; tmp;) {
+        struct info *next = tmp->next;
+        if (!next)
+            printf("           ['%s-PID<%d>:%s', %f]],\n", name, pid, tmp->name,
+                   tmp->nr);
+        else
+            printf("           ['%s-PID<%d>%s', %f],\n", name, pid, tmp->name,
+                   tmp->nr);
+        tmp = next;
+    }
+    printf("          \"piechart%d\");\n", chart_cnt++);
+}
+
+static void __print_info(struct info *head, const char *name, unsigned int pid)
+{
+    float time = 0;
+
+    if (strncmp(head->name, "ftrace", sizeof("ftrace")) == 0) {
+        print_per_task_ftrace_info(head, name, pid);
+        return;
+    }
+
+    printf("drawChart(");
+    printf("'%s-PID<%d>:", name, pid);
     for (int i = 0; i < head->top; i++)
         printf("%s:", head->stack_info[i]);
     printf("%s - %f us',\n", head->name, head->nr);
     printf("          [['%s', 'duration (us)'],\n", head->name);
+
     for (struct info *tmp = head->nested ? head->nested : head->next; tmp;) {
         struct info *next = tmp->next;
 
@@ -308,14 +369,14 @@ static void __print_info(struct info *head, unsigned int pid)
 }
 
 /* if [main function]->next->nr == 0, then it has nested_function */
-static void print_info(struct info *head, unsigned int pid)
+static void print_info(struct info *head, const char *name, unsigned int pid)
 {
     for (struct info *tmp = head->nested ? head->nested : head->next; tmp;
          tmp = tmp->next) {
         if (tmp->nested)
-            print_info(tmp, pid);
+            print_info(tmp, name, pid);
     }
-    __print_info(head, pid);
+    __print_info(head, name, pid);
 }
 
 // https://jsfiddle.net/linD026/a1fsxpkj/
@@ -332,7 +393,7 @@ int main(void)
     fclose(fp);
 
     for (int i = 0; i < task_top; i++)
-        print_info(tasks[i].head, tasks[i].pid);
+        print_info(tasks[i].head, tasks[i].name, tasks[i].pid);
     print_ftrace_info();
 
     printf("\n\nHTML source code:\n");
@@ -342,6 +403,10 @@ int main(void)
         printf(
             "       <div id=\"piechart%d\" style=\"width: 900px; height: 500px;\"></div>\n",
             i);
+
+    pr_info("[INFO] task:%d\n", task_top);
+    WARN_ON(task_top >= MAX_TASK_SIZE, "max task:%d/%d", task_top,
+            MAX_TASK_SIZE);
 
     return 0;
 }
